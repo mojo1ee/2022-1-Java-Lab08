ee9bdd42a040e11987c9f5e2782d89faa7cc65fe
Junhyung_Lee <22000576@handong.edu>
2022-05-10 02:52:11 +0900

    Gradle project and HW2 imported

---
+++ app/src/main/java/edu/handong/csee/java/hw2/MathDriver.java
@@ -0,0 +1,133 @@
+package edu.handong.csee.java.hw2;
+
+import edu.handong.csee.java.hw2.math.MathCalculator;
+
+/**
+ * This is a class for executing the MathCalculator class.
+ */
+public class MathDriver {
+    /**
+     * This is the main method which is the starting point of this program.
+     */
+    public static void main(String[] args){
+        MathDriver myDriver = new MathDriver();
+        myDriver.run(args);
+    }
+    
+    /**
+     * This is the run method that configures how each cases should be run depending on commandline arguments.
+     */
+    public void run(String[] args){
+
+        MathCalculator mathCalculator = new MathCalculator();
+
+        mathCalculator.setName(args[0]);
+
+        switch(args[1]){
+            case "Max":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +                    
+                                    mathCalculator.getMax(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+
+                break;
+            // TO DO: Fill the rest of cases below. Do not modify other parts of this file
+            case "Min":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +                    
+                                    mathCalculator.getMin(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+
+                break;
+
+    
+            case "Abs":
+            System.out.println("Driver Name: " + mathCalculator.getName() +
+                                ", Answer: " +                    
+                                mathCalculator.getAbs(Integer.parseInt(args[2])));
+
+            break;
+
+            case "Sum":
+                System.out.println("Driver Name: " + mathCalculator.getName() +
+                                    ", Answer: " +                    
+                                    mathCalculator.getSum(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+
+                break;
+
+            case "Diff":
+            System.out.println("Driver Name: " + mathCalculator.getName() +
+                                ", Answer: " +                    
+                                mathCalculator.getDiff(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+
+            break;
+
+            case "Product":
+            System.out.println("Driver Name: " + mathCalculator.getName() +
+                                ", Answer: " +                    
+                                mathCalculator.getProduct(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+
+            break;
+
+            case "Quotient":
+            if(Integer.parseInt(args[3])==0) System.out.println("Wrong input");
+            else{
+                    System.out.println("Driver Name: " + mathCalculator.getName() +
+                                        ", Answer: " +                    
+                                        mathCalculator.getQuotient(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+            }
+            break;
+
+            case "Remainder":
+            if(Integer.parseInt(args[3])==0) System.out.println("Wrong input");
+            else{
+            System.out.println("Driver Name: " + mathCalculator.getName() +
+                                ", Answer: " +                    
+                                mathCalculator.getRemainder(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+            }
+            break;
+
+            case "Power":
+            if(Integer.parseInt(args[3])<0) System.out.println("Wrong input"); //the func doesn't allow neg powers/
+            else{
+                    System.out.println("Driver Name: " + mathCalculator.getName() +
+                                        ", Answer: " +                    
+                                        mathCalculator.getPower(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+            }
+            break;
+
+            case "Factorial":
+            if(Integer.parseInt(args[2])<0) System.out.println("Wrong Input"); // by def of factorial, no negative inputs allowed.
+            else{
+                    System.out.println("Driver Name: " + mathCalculator.getName() +
+                                ", Answer: " +                    
+                                mathCalculator.getFactorial(Integer.parseInt(args[2])));
+            }
+            break;
+
+            case "Gcd":
+            System.out.println("Driver Name: " + mathCalculator.getName() +
+                                ", Answer: " +                    
+                                mathCalculator.getGcd(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+
+            break;
+
+            case "Lcm":
+            System.out.println("Driver Name: " + mathCalculator.getName() +
+                                ", Answer: " +                    
+                                mathCalculator.getLcm(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+
+            break;
+
+            case "Square":
+            System.out.println("Driver Name: " + mathCalculator.getName() +
+                                ", Answer: " +                    
+                                mathCalculator.getSquare(Integer.parseInt(args[2])));
+
+            break;
+
+
+            default:
+                System.out.println("Invalid command");
+        }
+
+    }
+}
---
+++ app/src/main/java/edu/handong/csee/java/hw2/math/MathCalculator.java
@@ -0,0 +1,212 @@
+package edu.handong.csee.java.hw2.math;
+
+/**
+ * This is a MathCalculator class that contains various math calculation methods.
+ * Following are the types of calcultation that this class provides. 
+ * 
+ * getMax, getMin, getAbs, getSum, getDiff, 
+ * getProduct, getQuotient, getRemainder, getPower, 
+ * getFactorial, getGGcd, getLcm, getSquare
+ */
+public class MathCalculator {
+    private String name;
+
+    /**
+     * This is a setter method for setting the name field.
+     */
+    public void setName (String name){
+        this.name = name;
+    }
+
+    /**
+     * This is a getter method for getting the name field. 
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * This is a method for getting the maximum value from the two integer input values. 
+     */
+    public int getMax(int num1, int num2){
+        if (num1 > num2) return num1;
+        else return num2;
+    }
+
+    /**
+     * This is a method for getting the mininmum value from the two integer input values. 
+     */
+    public int getMin(int num1, int num2){
+        if (num1 < num2) return num1;
+        else return num2;
+    }
+
+    /**
+     * This is a method for getting the absolute value of the integer input value.
+     */
+    public int getAbs(int num){
+        if (num <0) num = -num;
+
+        return num;
+    }
+
+    /**
+     * This is a method for getting the summation of the two integer input values. 
+     */
+    public int getSum(int num1, int num2){
+        int sum = num1 + num2;
+
+        return sum;
+    }
+
+    /**
+     * This is a method for getting the difference between the two integer input values. 
+     */
+    public int getDiff(int num1, int num2){
+
+        int diff = num1 - num2;
+
+        return diff;
+    }
+
+    /**
+     * This is a method for getting the product of the two integer input values. 
+     */
+    public int getProduct(int num1, int num2){
+        int product = num1 * num2;
+
+        return product;
+    }
+    
+    /**
+     * This is a method for getting the Quotient of integer input num1 divided by integer input num2
+     * Produces an error message when num2 is 0
+     */
+    public int getQuotient(int num1, int num2){
+        int quotient = num1 / num2;
+
+        return quotient;
+    }
+
+    /**
+     * This is a method for getting the Remainder of integer input num1 divied by integer input num2
+     *  Produces an error message when num2 is 0
+     */
+    public int getRemainder(int num1, int num2){
+        int remainder=-1;
+
+        if(num2 ==0) return remainder; //wrong input;
+        else remainder = num1 % num2;
+
+        return remainder;
+    }
+
+    /**
+     * This is a method for getting the result of exponentiating the first integer input num1 to the power of the second integer input num2
+     */
+    public int getPower(int num1, int num2){
+        int power; 
+        int i; 
+
+        if(num2 == 0) return 1;
+        else if (num2 < 0) return 0; // since it's an int type func => doens't allow negative powers
+        else {
+            power = num1;
+            for(i=1;i<num2;i++){
+                power = power * num1;
+            }
+        }
+        return power;
+    }
+
+    /**
+     * This is a method for getting the factorial result of the integer input num.
+     */
+    public int getFactorial(int num){
+        int fact;
+        int temp=num;
+        int multiplier =num-1;
+
+        
+        if (num<0) return -1; //wrong input
+        else if (num ==0) return 1;
+        else if(num==1) return 1;
+
+        while(multiplier>=1) {
+            
+            temp = temp*multiplier;
+            multiplier--;
+        }
+        fact = temp;
+
+        return fact;
+    }
+
+    /**
+     * This is a method for getting the greatest common denominator of the two integer inputs num1 and num2.
+     */
+    public int getGcd(int num1, int num2){
+        int gcd=1; 
+        int temp;
+        int i;
+        int smaller_num = (num1<num2) ? num1:num2;
+        int bigger_num = (num1>num2) ? num1:num2;
+
+        if(num1 == num2) gcd = num1;
+        else if (bigger_num % smaller_num == 0)
+            gcd = smaller_num;
+        else{
+            for(i = 1; i < smaller_num; i++){
+                if(i > smaller_num/2) break;
+
+                if(smaller_num % i ==0){
+                    temp =i;
+                    if(bigger_num % temp ==0){
+                        gcd = temp;
+                    }
+                }
+            }
+        }
+
+        return gcd;
+    }
+
+    /**
+     * This is a method for getting the least common multiple of the two integer inputs num1 and num2.
+     */
+    public int getLcm(int num1, int num2){
+        int lcm=0;
+        int multiplier;
+        int temp;
+        int smaller_num = (num1<num2) ? num1:num2;
+        int bigger_num = (num1>num2) ? num1:num2;
+
+        if(num1 == num2) {
+            lcm = num1;
+            return num1; 
+        }
+
+        multiplier = 1;
+        while(true){
+            temp = bigger_num * multiplier;
+            if(temp % smaller_num ==0) {
+                lcm = temp;
+                break;
+            }
+            multiplier++;
+        }
+
+        return lcm; 
+    }
+
+    /**
+     * This is a method for getting the squared value of the integer input num.
+     */
+    public int getSquare(int num){
+        int square;
+
+        square = num*num;
+
+        return square;
+    }
+}
